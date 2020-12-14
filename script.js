3cb4df7cf042a030677e27ac5bd4ad19f3e00af2
“Awanee <Awanee.pandey@gmail.com>
2020-12-14 17:49:04 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const progress = document.getElementById('progress');
+const circles = document.querySelectorAll('.circle');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next'); 
+
+let currentActive = 1;
+function update(){
+    circles.forEach((circle,index)=>{
+        if(index <currentActive ){
+            circle.classList.add('active');
+        }else{
+            circle.classList.remove('active');
+        }
+    })
+    const activeCircles = document.querySelectorAll('.active')
+    progress.style.width = (activeCircles.length - 1)/(circles.length-1) * 100 + '%';
+    if(currentActive === 1){
+        prev.disabled = true;
+    }else if(currentActive === circles.length){
+        next.disabled = true ;
+    }else{
+        prev.disabled = false;
+        next.disabled = false;
+    }
+}
+console.log(circles);
+next.addEventListener('click',()=>{
+    currentActive++;
+  
+    if(currentActive>circles.length){
+        currentActive = circles.length;
+    }
+    update();
+})
+
+prev.addEventListener('click',()=>{
+    currentActive--;
+    if(currentActive<1){
+        currentActive = 1;
+    }
+    console.log(currentActive);
+    update();
+})
